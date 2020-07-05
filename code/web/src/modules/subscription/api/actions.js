d79701b081bffaf6afeaa33ad33cc8bc8d08795b
Jeremy Poulter <Jpoulter@gmail.com>
2020-07-05 12:01:28 -0600

    Revise annotations to be more accurate/specific

--- code/web/src/modules/subscription/api/actions.js
+++ code/web/src/modules/subscription/api/actions.js
@@ -17,12 +17,13 @@ export const SUBSCRIPTIONS_GET_RESPONSE = 'SUBSCRIPTIONS/GET_RESPONSE'
 export const SUBSCRIPTIONS_GET_FAILURE = 'SUBSCRIPTIONS/GET_FAILURE'
 
 // Actions
-// Actions are methods that carry information payloads used to update our application's global store
+// Actions are methods that carry information payloads used to determine which reducer to use and 
+// what information to pass along with it.  
 
 // Get list of subscriptions
 export function getList(isLoading = true) {
-{/*This action starts off by updating the global store with a isLoading property and through the type,
-   we can see what type of action is loading. */}
+  {/*This action starts off by accessing the 'SUBSCRIPTIONS_GET_LIST_REQUEST' reducer with an isLoading 
+  property of true*/}
   return dispatch => {
     dispatch({
       type: SUBSCRIPTIONS_GET_LIST_REQUEST,
@@ -32,9 +33,9 @@ export function getList(isLoading = true) {
 {/* This Axios post, which is similar to a fetch request, is making a query to the GraphQL schema.
     It's making a query to the subscriptions category and requesting back the properties specified
     in the fields array. If the query is successful and we get back a 200 response code, then
-    update the global store isLoading property to false and update the store with the list
-    we are returned.  If it fails, the failure action type will return the associated error
-    message but will also set the isLoading property to false.*/}
+    use the 'SUBSCRIPTIONS_GET_LIST_RESPONSE' reducer with a payload including an isLoading property 
+    to false a list of subscriptions that we are returned.  If it fails, use the 'SUBSCRIPTIONS_GET_LIST_FAILURE'
+    reducer with a payload of an isLoading property of false and a property of error with a message*/}
     return axios.post(routeApi, query({
       operation: 'subscriptions',
       fields: ['id', 'user { name, email }', 'crate { id, name, description }', 'createdAt']
@@ -66,7 +67,8 @@ export function getList(isLoading = true) {
 // This function operates in the same way as the getList function, but the difference is that is makes
 // the query to the subscriptionsByUser query rather than the subscriptions query.  Both of these queries
 // live in the same file, and return to us a GraphQLList, but this one will get us back the subscriptions
-// that the user has.
+// that the user has.  Another difference is that it uses a different set of associated reducers which 
+// operate in a similar way.
 export function getListByUser(isLoading = true) {
   return dispatch => {
     dispatch({
@@ -104,8 +106,9 @@ export function getListByUser(isLoading = true) {
 // Get single subscription
 // This function operates similarly to the two functions above, but it will be getting back a single
 // subscription rather than a list of subscriptions.  The particular subscription it is requesting to the subscription
-// query is determined by the 'id' argument that is passed in.  If the query is successful, it will update the global
-// store property 'subscription' and in that object it will be updating the 'item' property.
+// query is determined by the 'id' argument that is passed in.  If the query is successful, it will reference the
+// 'SUBSCRIPTIONS_GET_RESPONSE' reducer with the item payload and the relevant isLoading/error values.  If it's 
+// unsuccessful it will referene the 'SUBSCRIPTIONS_GET_FAILURE' reducer with relevant isLoading/error values.
 export function get(slug, isLoading = true) {
   return dispatch => {
     dispatch({

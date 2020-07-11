ac7163e5b5fbd291a67cf0ac2806d926db5ffbb6
Jeremy Poulter <Jpoulter@gmail.com>
2020-07-10 22:43:32 -0600

    Adjust styling of shipments component and add page title area

--- code/web/src/modules/user/Shipments.js
+++ code/web/src/modules/user/Shipments.js
@@ -7,31 +7,52 @@ import { Link } from 'react-router-dom'
 
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
-import { H3, H4 } from '../../ui/typography'
-import Button from '../../ui/button'
-import Icon from '../../ui/icon'
-import { white, black } from '../../ui/common/colors'
+import { H2, H3, H4, H5 } from '../../ui/typography'
+import Card from '../../ui/card/Card'
+import { white, black, grey, grey2 } from '../../ui/common/colors'
 
-// App Imports
-import userRoutes from '../../setup/routes/user'
-
-
-const Shipments = (props) => (
+const Shipments = () => (
     <div>
-      <Grid alignCenter={true} style={{ padding: '2em' }}>
-        {/* SEO */}
-        <Helmet>
-          <title> My Subscriptions - Crate</title>
-        </Helmet>
+      {/* SEO */}
+      <Helmet>
+        <title> My Shipments - Crate</title>
+      </Helmet>
 
+      {/* Top title bar */}
+      <Grid style={{ backgroundColor: grey }}>
+        <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+          <H3 font="secondary">Your Shipments</H3>
+          <p style={{ marginTop: '1em', color: grey2 }}>View all your past/future shipments, the items it contains, and which items you kept!</p>
+        </GridCell>
+      </Grid>
+
+      <Grid>
+      
         {/* Left Content - Shipment Cards */}
-        <GridCell>
-
+        <GridCell alignCenter={true} justifyCenter={true} style={{maxWidth: '30em'}}>
+          <Card style={{ alignSelf: 'center', width: '20em', backgroundColor: white, margin: '2em', textAlign: 'center', padding: '2em' }}>
+            <H4 style={{ color: black}}>Clothes for Men</H4>
+            <p style={{ marginTop: '2em' }}>Arrived on January 3rd</p>
+          </Card>
+          <Card style={{ width: '20em', backgroundColor: white, margin: '2em', textAlign: 'center', padding: '2em' }}>
+            <H4 style={{ color: black}}>Accessories for Women</H4>
+            <p style={{ marginTop: '2em' }}>To Arrive on January 30th</p>
+          </Card>
         </GridCell>
 
         {/* Right Content - Shipment Card Details */}
-        <GridCell>
-  
+        <GridCell style={{ display: 'flex', justifyContent: 'center' }}>
+          <Card style={{ width: '50em', marginTop: '1em', padding: '1em', backgroundColor: white }}>
+            <H2 style={{border: '1px solid black', marginBottom: '0.3em', width: '100%', textAlign: 'center'}}>Clothes for Men</H2>
+            <p style={{ color: black, width: '100%', textAlign: 'center' }}>(Arrival Date <u>January 3rd</u>)</p>
+            <H3 style={{ paddingLeft: '1em', color: black, marginTop: '1em'}}>Items In This Order:</H3>
+            <ul style={{marginTop: '2em'}}>
+              <li style={{ paddingLeft: '3em', marginTop: '1em' }}>Cool Hat 🧢</li>
+              <li style={{ paddingLeft: '3em', marginTop: '1em' }}>Cool Pants 👖</li>
+              <li style={{ paddingLeft: '3em', marginTop: '1em' }}>Cool Shirt 👔</li>
+              <li style={{ paddingLeft: '3em', marginTop: '1em' }}>Cool Watch ⌚️</li>
+            </ul>
+          </Card>
         </GridCell>
 
       </Grid>
@@ -44,10 +65,10 @@ Shipments.propTypes = {
 }
 
 // Component State
-function profileState(state) {
+function shipmentsState(state) {
   return {
     user: state.user
   }
 }
 
-export default connect(profileState, {})(Shipments)
+export default connect(shipmentsState, {})(Shipments)

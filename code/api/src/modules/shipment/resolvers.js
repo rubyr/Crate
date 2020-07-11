4591d6adf635ac19c7b530aa8c15484660e1a932
Kevin McGrevey <niv3kmcg@gmail.com>
2020-07-10 20:47:01 -0600

    Adds functionality for query to return shipmentById

--- code/api/src/modules/shipment/resolvers.js
+++ code/api/src/modules/shipment/resolvers.js
@@ -5,3 +5,15 @@ import models from '../../setup/models'
 export async function getAll() {
   return await models.Shipment.findAll({ order: [['id', 'DESC']] })
 }
+
+// Get shipment by ID
+export async function getById(parentValue, { shipmentId }) {
+  const shipment = await models.Shipment.findOne({ where: { id: shipmentId } })
+
+  if (!shipment) {
+    // Shipment does not exists
+    throw new Error('The shipment you are looking for does not exists or has been deleted.')
+  } else {
+    return shipment
+  }
+}
